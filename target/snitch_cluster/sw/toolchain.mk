090ab269977f471ba82c5a4bea1b5e24c3a72475
Paul Scheffler <paulsc@iis.ee.ethz.ch>
2023-07-18 14:12:13 +0200

    MUSL: Adding a new lightweight math library to Snitch (#25)
    * musl: add library as new submodule with README
    
    * musl: add musl library to toolchain
    
    * ci: add musl build
    
    ---------
    
    Co-authored-by: Viviane Potocnik <vivianep@iis.ee.ethz.ch>

--- target/snitch_cluster/sw/toolchain.mk
+++ target/snitch_cluster/sw/toolchain.mk
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Luca Colagrande <colluca@iis.ee.ethz.ch>
+# Viviane Potocnik <vivianep@iis.ee.ethz.ch>
 
 ######################
 # Invocation options #
@@ -13,6 +14,10 @@ DEBUG ?= OFF # ON to turn on debugging symbols
 ###################
 # Build variables #
 ###################
+
+# Directory to the MUSL installation
+MUSL_DIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
+SW_INSTALL_DIR  ?= $(abspath $(MUSL_DIR)/../../../sw/deps/install/)
 
 # Compiler toolchain
 LLVM_BINROOT    ?= $(dir $(shell which riscv32-unknown-elf-clang))
@@ -25,6 +30,11 @@ RISCV_DWARFDUMP ?= $(LLVM_BINROOT)/llvm-dwarfdump
 
 # Compiler flags
 RISCV_CFLAGS += $(addprefix -I,$(INCDIRS))
+
+# musl specific flags
+RISCV_CFLAGS += -I$(SW_INSTALL_DIR)/include
+RISCV_CFLAGS += -B$(SW_INSTALL_DIR)/bin
+
 RISCV_CFLAGS += -mcpu=snitch
 RISCV_CFLAGS += -menable-experimental-extensions
 RISCV_CFLAGS += -mabi=ilp32d
@@ -33,6 +43,13 @@ RISCV_CFLAGS += -ffast-math
 RISCV_CFLAGS += -fno-builtin-printf
 RISCV_CFLAGS += -fno-common
 RISCV_CFLAGS += -fopenmp
+
+# TODO: check why the LTO flags optimizes the CBSS section away
+# RISCV_CFLAGS += -flto=thin
+
+RISCV_CFLAGS += -ftls-model=local-exec
+RISCV_CFLAGS += -nostdinc
+
 RISCV_CFLAGS += -O3
 ifeq ($(DEBUG), ON)
 RISCV_CFLAGS += -g
@@ -42,6 +59,10 @@ endif
 RISCV_LDFLAGS += -fuse-ld=$(RISCV_LD)
 RISCV_LDFLAGS += -nostartfiles
 RISCV_LDFLAGS += -lm
+# musl specific flags
+RISCV_LDFLAGS += -L$(SW_INSTALL_DIR)/lib
+RISCV_LDFLAGS += -nostdinc
+# RISCV_LDFLAGS += -flto=thin
 
 # Archiver flags
 RISCV_ARFLAGS = rcs
