22b601c524e041c1212b872677fc721799d6df58
xiaoling-yi <143962462+xiaoling-yi@users.noreply.github.com>
2023-09-15 15:33:47 +0200

    Add HWPE MAC files (#12)
    * hw: Add HWPE MAC ctrl code
    
    * hw: Add HWPE MAC reqrsp code
    
    * hw: Add HWPE MAC top code
    
    * bender: add HWPE mac files to bender list
    
    * lint: Exclude HWPE MAC files from license checker
    
    * hw: make sure the module name matches the fine name
    
    * bender: change the file name of hwpe mac
    
    * hw: rm the hwpe mac file with wrong filename
    
    * bender: change the order of hwpe mac in bender list
    
    * hw: edit comments
    
    * hw: Clarify comments about the data addressing
    
    ---------
    
    Co-authored-by: rgantonio <ryan.antonio@esat.kuleuven.be>

---
+++ hw/snax_hwpe_mac/src/snax_hwpe_ctrl.sv
@@ -0,0 +1,286 @@
+// Copyright 2023 Katolieke Universiteit Leuven (KUL)
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+
+// Author: Ryan Antonio (ryan.antonio@kuleuven.be)
+
+// verilog_lint: waive-start line-length
+// verilog_lint: waive-start no-trailing-spaces
+
+import riscv_instr::*;
+
+module snax_hwpe_ctrl #(
+  parameter int unsigned DataWidth  = 64,
+  parameter type acc_req_t          = logic,      // Memory request payload type, usually write enable, write data, etc.
+  parameter type acc_rsp_t          = logic       // Memory response payload type, usually read data
+)(
+  input  logic                      clk_i,        // Clock
+  input  logic                      rst_ni,       // Asynchronous reset, active low
+  input  acc_req_t                  req_i,        // Request stream interface, payload
+  input  logic                      req_valid_i,  // Request stream interface, payload is valid for transfer
+  output logic                      req_ready_o,  // Request stream interface, payload can be accepted
+  output acc_rsp_t                  resp_o,       // Response stream interface, payload
+  output logic                      resp_valid_o, // Response stream interface, payload is valid for transfer
+  input  logic                      resp_ready_i, // Response stream interface, payload can be accepted
+  hwpe_ctrl_intf_periph.master      periph        // periph slave port
+);
+
+  //---------------------------------------------
+  // Some important notes:
+  // - The periph interface is fixed to 32 bits only
+  // - That's the assumption of the HWPE
+  //---------------------------------------------
+
+  //---------------------------------------------
+  // Local parameters
+  //---------------------------------------------
+
+  // Number of bits to fill to extend to DataWidth
+  localparam int unsigned FillBits = DataWidth - 32;
+
+  //---------------------------------------------
+  // Registers and wires
+  //---------------------------------------------
+
+  // These signals are decoded and come from
+  // the acc_reqrsp signals
+  logic       req;
+  logic       wen;
+  logic [3:0] be;
+
+  // Typedef struct for pack and unpack signals
+  typedef struct packed {
+    logic [ 4:0] id;
+    logic        req;
+    logic [31:0] add;
+    logic        wen;
+    logic        be;
+    logic [31:0] data;
+  } hwpe_tcdm_t;
+
+  typedef struct packed {
+    logic [ 4:0] r_id;
+    logic        r_valid;
+    logic [31:0] r_data;
+  } tcdm_hwpe_t;
+
+  hwpe_tcdm_t sn_hwpe_reg;
+
+  // This is just a necessary wiring to re-map the data going
+  // back to acc_reqrsp to 64 bits or anything beyond 32 bits
+  logic [31:0] unpacked_data;
+
+  //---------------------------------------------
+  // Combinational logic and wiring assignments
+  // for SN to HWPE FIFO direction
+  // In this control we do a 1-step register buffer
+  //---------------------------------------------
+  logic transaction_start;
+  logic transaction_end;
+  logic is_write;
+  logic is_read;
+
+  // A transaction is valid when both ready and valid signal for requestor are valid
+  assign transaction_start = req_valid_i & req_ready_o;
+  assign transaction_end   = periph.req & periph.gnt;
+  assign is_write          = transaction_start & !wen;
+  assign is_read           = transaction_start &  wen;
+
+  // wen = 1'b1 whenever we read. wen = 1'b0 whenever we write
+  // decode this based on the instruction given
+  // Need to use unique casez to ignore `?` bits
+  always_comb begin
+    unique casez (req_i.data_op)
+      CSRRS, CSRRSI, CSRRC, CSRRCI: begin
+        wen = 1'b1;
+      end
+      default: begin
+        wen = 1'b0;
+      end
+    endcase
+  end
+
+  // Adding switcher to handle both instruction extension and CSR
+  logic [31:0] address_in;
+
+  always_comb begin
+    unique casez (req_i.data_op)
+      CSRRW, CSRRWI, CSRRS, CSRRSI, CSRRC, CSRRCI: begin
+        //Offset due to start of address CSR and << 2 due to "byte" addressable of registers
+        address_in = (req_i.data_argb[31:0] - 32'd960) << 2; 
+      end
+      default: begin
+        address_in = req_i.data_argb[31:0];
+      end
+    endcase
+  end
+
+  // Byte enable always only when we need to write
+  assign be  = (is_write) ? 4'hF : 4'h0;
+
+
+  // States
+  typedef enum logic [1:0] {
+    WAIT,
+    WRITE,
+    READ
+  } ctrl_states_t;
+
+  
+  ctrl_states_t cstate, nstate;
+
+  // Changing states
+  always_ff @ (posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      cstate <= WAIT;
+    end else begin
+      cstate <= nstate;
+    end
+  end
+
+  // Next state changes
+  always_comb begin
+    case(cstate)
+      WAIT: begin
+        if (is_write) begin
+          nstate = WRITE;
+        end else if (is_read) begin
+          nstate = READ;
+        end else begin
+          nstate = WAIT;
+        end
+      end 
+      WRITE: begin
+        if (transaction_end) begin 
+          nstate = WAIT; 
+        end else begin
+          nstate = WRITE; 
+        end
+      end
+      READ: begin
+        if (periph.r_valid) begin 
+          nstate = WAIT; 
+        end else begin
+          nstate = READ; 
+        end
+      end
+      default: begin
+        nstate = WAIT;
+      end
+    endcase
+
+  end
+
+  //---------------------------------------------
+  // Combinational logic and wiring assignments
+  // for HWPE to SN FIFO queue
+  //---------------------------------------------
+
+
+  // Simply extending the unpacked_data to 64 bits
+  // At the same time wiring it to the resp_o.data value
+  // We make a condition here just for safety if DataWidth is less than 32
+  if (FillBits > 0) begin: gen_fill_bits
+
+    // Fill upper bits with 0s
+    assign unpacked_data = {{FillBits{1'b0}},periph.r_data};
+
+  end else begin: gen_no_fill_bits
+
+    // This automatically truncates the upper bits
+    // if the resp_o.data is more than 32 bits.
+    assign unpacked_data = periph.r_data;
+
+  end
+ 
+  // Controller
+  always_ff @ (posedge clk_i or negedge rst_ni) begin
+    if(!rst_ni) begin
+      req_ready_o   <= 1'b1;
+      periph.id     <= '0;
+      periph.req    <= '0;
+      periph.add    <= '0;
+      periph.wen    <= '0;
+      periph.be     <= '0;
+      periph.data   <= '0;
+    end else begin
+
+      case (cstate)
+        WAIT: begin
+          if (is_write || is_read) begin 
+            req_ready_o <= 1'b0;
+            periph.id   <= req_i.id;
+            periph.req  <= 1'b1;
+            periph.add  <= address_in;
+            periph.wen  <= wen;
+            periph.be   <= be;
+            periph.data <= req_i.data_arga[31:0];
+          end
+        end 
+        WRITE: begin 
+          if (transaction_end) begin 
+            req_ready_o <= 1'b1;
+            periph.id   <= '0;
+            periph.req  <= '0;
+            periph.add  <= '0;
+            periph.wen  <= '0;
+            periph.be   <= '0;
+            periph.data <= '0;
+          end
+        end
+        READ: begin
+          if (periph.r_valid) begin
+            req_ready_o <= 1'b1;
+            periph.id   <= '0;
+            periph.req  <= '0;
+            periph.add  <= '0;
+            periph.wen  <= '0;
+            periph.be   <= '0;
+            periph.data <= '0;
+          end
+        end
+        default: begin
+          req_ready_o   <= 1'b1;
+          periph.id     <= '0;
+          periph.req    <= '0;
+          periph.add    <= '0;
+          periph.wen    <= '0;
+          periph.be     <= '0;
+          periph.data   <= '0;
+        end
+      endcase
+    end
+  end
+  
+  // Response port
+  always_ff @ (posedge clk_i or negedge rst_ni) begin
+    if(!rst_ni) begin
+      resp_o.id     <= '0;
+      resp_o.error  <= '0;
+      resp_o.data   <= '0;
+      resp_valid_o  <= 1'b0;
+    end else begin
+
+      case (cstate)
+        READ: begin
+          if (periph.r_valid) begin 
+            resp_o.id     <= periph.r_id;
+            resp_o.error  <= '0;
+            resp_o.data   <= unpacked_data;
+            resp_valid_o  <= 1'b1;
+          end
+        end 
+        default: begin
+          resp_o.id     <= '0;
+          resp_o.error  <= '0;
+          resp_o.data   <= '0;
+          resp_valid_o  <= 1'b0;
+        end
+      endcase
+    end
+  end
+
+// verilog_lint: waive-stop line-length
+// verilog_lint: waive-stop no-trailing-spaces
+
+endmodule
---
+++ hw/snax_hwpe_mac/src/snax_mac.sv
@@ -0,0 +1,176 @@
+// Copyright 2023 Katolieke Universiteit Leuven (KUL)
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+
+// Author: Ryan Antonio (ryan.antonio@kuleuven.be)
+
+// verilog_lint: waive-start line-length
+// verilog_lint: waive-start no-trailing-spaces
+
+module snax_mac # (
+  parameter int unsigned DataWidth         = 32,
+  parameter int unsigned SnaxTcdmPorts = 4,
+  parameter type         acc_req_t         = logic,
+  parameter type         acc_rsp_t         = logic,
+  parameter type         tcdm_req_t        = logic,
+  parameter type         tcdm_rsp_t        = logic
+)(
+  input     logic                               clk_i,
+  input     logic                               rst_ni,
+  input     logic                               snax_qvalid_i,
+  output    logic                               snax_qready_o,
+  input     acc_req_t                           snax_req_i,
+  output    acc_rsp_t                           snax_resp_o,
+  output    logic                               snax_pvalid_o,
+  input     logic                               snax_pready_i,
+  output    tcdm_req_t  [SnaxTcdmPorts-1:0] snax_tcdm_req_o,
+  input     tcdm_rsp_t  [SnaxTcdmPorts-1:0] snax_tcdm_rsp_i
+);
+  //------------------------------
+  // HWPE control interface
+  //------------------------------
+  hwpe_ctrl_intf_periph #(
+      .ID_WIDTH ( 5 )
+  ) snax_periph (
+      .clk ( clk_i )
+  );
+
+  hwpe_stream_intf_tcdm snax_tcdm [SnaxTcdmPorts-1:0] (
+      .clk ( clk_i )
+  );
+
+  //------------------------------
+  // Some brute force typedefs and wiring
+  //------------------------------
+  typedef logic [31:0] hwpe_mem_addr_t;
+  typedef logic [31:0] mem_data_t;
+  typedef logic [ 3:0] mem_strb_t;
+
+  typedef struct packed {
+    logic           [SnaxTcdmPorts-1:0]  req;
+    logic           [SnaxTcdmPorts-1:0]  gnt;
+    hwpe_mem_addr_t [SnaxTcdmPorts-1:0]  add;
+    logic           [SnaxTcdmPorts-1:0]  wen;
+    mem_strb_t      [SnaxTcdmPorts-1:0]  be;
+    mem_data_t      [SnaxTcdmPorts-1:0]  data;
+    mem_data_t      [SnaxTcdmPorts-1:0]  r_data;
+    logic           [SnaxTcdmPorts-1:0]  r_valid;
+    logic                                r_opc;
+    logic                                r_user;
+  } loc_mem_t;
+
+  loc_mem_t snax_mem;
+
+  //------------------------------
+  // SNAX HWPE controller
+  // Translates the snax valid-ready signals
+  // Into HWPE signals
+  //------------------------------
+  snax_hwpe_ctrl #(
+    .DataWidth    ( DataWidth          ), // Default data width
+    .acc_req_t    ( acc_req_t          ), // Memory request payload type, usually write enable, write data, etc.
+    .acc_rsp_t    ( acc_rsp_t          )  // Memory response payload type, usually read data
+  ) i_snax_hwpe_ctrl (
+    .clk_i        ( clk_i              ), // Clock
+    .rst_ni       ( rst_ni             ), // Asynchronous reset, active low
+    .req_i        ( snax_req_i         ), // Request stream interface, payload
+    .req_valid_i  ( snax_qvalid_i      ), // Request stream interface, payload is valid for transfer
+    .req_ready_o  ( snax_qready_o      ), // Request stream interface, payload can be accepted
+    .resp_o       ( snax_resp_o        ), // Response stream interface, payload
+    .resp_valid_o ( snax_pvalid_o      ), // Response stream interface, payload is valid for transfer
+    .resp_ready_i ( snax_pready_i      ), // Response stream interface, payload can be accepted
+    .periph       ( snax_periph        )  // periph master port
+  );
+
+  //------------------------------
+  // Main MAC engine
+  //------------------------------
+  snax_mac_top_wrap #(
+      .N_CORES      ( 1                   ),
+      .MP           ( SnaxTcdmPorts       ),
+      .ID           ( 5                   )
+  ) i_mac_top (
+    .clk_i          ( clk_i               ),
+    .rst_ni         ( rst_ni              ),
+    .test_mode_i    ( 1'b0                ),
+    .evt_o          (                     ),      // Unused
+    .tcdm_req       ( snax_mem.req        ),
+    .tcdm_gnt       ( snax_mem.gnt        ),      // input
+    .tcdm_add       ( snax_mem.add        ),
+    .tcdm_wen       ( snax_mem.wen        ),
+    .tcdm_be        ( snax_mem.be         ),
+    .tcdm_data      ( snax_mem.data       ),
+    .tcdm_r_data    ( snax_mem.r_data     ),      // input
+    .tcdm_r_valid   ( snax_mem.r_valid    ),      // input
+    .periph_req     ( snax_periph.req     ),
+    .periph_gnt     ( snax_periph.gnt     ),
+    .periph_add     ( snax_periph.add     ),
+    .periph_wen     ( snax_periph.wen     ),
+    .periph_be      ( snax_periph.be      ),
+    .periph_data    ( snax_periph.data    ),
+    .periph_id      ( snax_periph.id      ),
+    .periph_r_data  ( snax_periph.r_data  ),
+    .periph_r_valid ( snax_periph.r_valid ),
+    .periph_r_id    ( snax_periph.r_id    )
+  );
+
+  //------------------------------
+  // Manual remapping and porting of each
+  // Streamer to a TCDM with valid-ready response
+  // This part goes into the local memory
+  //------------------------------
+  genvar i;
+
+  for (i = 0; i < SnaxTcdmPorts; i++) begin: gen_map_translate
+
+    assign snax_tcdm       [i].req  = snax_mem.req [i];
+    assign snax_mem.gnt    [i]      = snax_tcdm    [i].gnt;
+    assign snax_tcdm       [i].add  = snax_mem.add [i];
+    assign snax_tcdm       [i].wen  = snax_mem.wen [i];
+    assign snax_tcdm       [i].be   = snax_mem.be  [i];
+    assign snax_tcdm       [i].data = snax_mem.data[i];
+    assign snax_mem.r_data [i]      = snax_tcdm    [i].r_data;
+    assign snax_mem.r_valid[i]      = snax_tcdm    [i].r_valid;
+
+    snax_hwpe_to_reqrsp #(
+      .DataWidth        ( DataWidth           ),  // Data width to use
+      .tcdm_req_t       ( tcdm_req_t          ),  // TCDM request type
+      .tcdm_rsp_t       ( tcdm_rsp_t          )   // TCDM response type
+    ) i_snax_hwpe_to_reqrsp (
+      .clk_i            ( clk_i               ),  // Clock
+      .rst_ni           ( rst_ni              ),  // Asynchronous reset, active low
+      .tcdm_req_o       ( snax_tcdm_req_o[i]  ),  // TCDM valid ready format
+      .tcdm_rsp_i       ( snax_tcdm_rsp_i[i]  ),  // TCDM valid ready format
+      .hwpe_tcdm_slave  ( snax_tcdm[i]        )   // HWPE TCDM slave port
+    );
+
+  end
+
+// verilog_lint: waive-stop line-length
+// verilog_lint: waive-stop no-trailing-spaces
+
+endmodule
+
+/*---------- Module Usage ----------
+
+snax_mac # (
+  .DataWidth          ( DataWidth         ),
+  .SnaxTcdmPorts      ( SnaxTcdmPorts     ),
+  .acc_req_t          ( acc_req_t         ),
+  .acc_rsp_t          ( acc_rsp_t         )
+  .tcdm_req_t         ( tcdm_req_t        ),
+  .tcdm_rsp_t         ( tcdm_rsp_t        )
+) i_snax_mac (
+  .clk_i              ( clk_i             ),
+  .rst_ni             ( rst_ni            ),
+  .snax_qvalid_i      ( snax_qvalid_i     ),
+  .snax_qready_o      ( snax_qready_o     ),
+  .snax_req_i         ( snax_req_i        ),
+  .snax_resp_o        ( snax_resp_o       ),
+  .snax_pvalid_o      ( snax_pvalid_o     ),
+  .snax_pready_i      ( snax_pready_i     ),
+  .snax_tcdm_req_o    ( snax_tcdm_req_o   ),
+  .snax_tcdm_rsp_i    ( snax_tcdm_rsp_i   )
+);
+
+------------------------------------*/
