1a44dec1d2c52b95c02c5070c5e1afbb3d5c7ac6
Ryan Antonio <ryan.antonio@esat.kuleuven.be>
2023-10-10 15:29:12 +0200

    Update HWPE MAC to use 32-bits only (#32)
    * hw: Update SNAX control for 32-bit support
    
    * hw: Update SNAX hwpe to reqrsp to support 32-bits
    
    * hw: Take out unused parameter
    
    * sw: Move uint64_t to uint32_t
    
    * sw: Take out unnecessary debug code
    
    * sw: Add new line to data.h

--- target/snitch_cluster/sw/apps/snax-mac/src/snax-mac.c
+++ target/snitch_cluster/sw/apps/snax-mac/src/snax-mac.c
@@ -10,12 +10,12 @@ int main() {
     // Set err value for checking
     int err = 0;
 
-    uint64_t final_output;
+    uint32_t final_output;
 
-    uint64_t *local_a, *local_b, *local_c, *local_o;
+    uint32_t *local_a, *local_b, *local_c, *local_o;
 
     // Allocate space in TCDM
-    local_a = (uint64_t *)snrt_l1_next();
+    local_a = (uint32_t *)snrt_l1_next();
     local_b = local_a + VEC_LEN;
     local_c = local_b + VEC_LEN;
     local_o = local_c + 1;
@@ -24,8 +24,8 @@ int main() {
 
     // Use data mover core to bring data from L3 to TCDM
     if (snrt_is_dm_core()) {
-        size_t vector_size = VEC_LEN * sizeof(uint64_t);
-        size_t scale_size = 1 * sizeof(uint64_t);
+        size_t vector_size = VEC_LEN * sizeof(uint32_t);
+        size_t scale_size = 1 * sizeof(uint32_t);
         snrt_dma_start_1d(local_a, A, vector_size);
         snrt_dma_start_1d(local_b, B, vector_size);
         snrt_dma_start_1d(local_c, &C, scale_size);
@@ -43,10 +43,10 @@ int main() {
         uint32_t csr_set = snrt_mcycle();
 
         // Set addresses
-        write_csr(0x3d0, (uint64_t)local_a);
-        write_csr(0x3d1, (uint64_t)local_b);
-        write_csr(0x3d2, (uint64_t)local_c);
-        write_csr(0x3d3, (uint64_t)local_o);
+        write_csr(0x3d0, (uint32_t)local_a);
+        write_csr(0x3d1, (uint32_t)local_b);
+        write_csr(0x3d2, (uint32_t)local_c);
+        write_csr(0x3d3, (uint32_t)local_o);
 
         // Set configs
         write_csr(0x3d4, 1);   // Number of iterations
