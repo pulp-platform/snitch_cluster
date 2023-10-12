b806f87332eb7f0dada815852c8163e2787bb8dd
Josse Van Delm <josse.vandelm@gmail.com>
2023-10-12 14:37:07 +0200

    sw: Refactor correctness check

--- target/snitch_cluster/sw/apps/snax-mac-tiled/src/snax-mac-tiled.c
+++ target/snitch_cluster/sw/apps/snax-mac-tiled/src/snax-mac-tiled.c
@@ -18,31 +18,51 @@ void snax_mac_sw_barrier(){
 
 void snax_mac_setup_simple_mult(uint32_t* a, uint32_t* b,
                                 uint32_t* o, uint32_t vector_length){
-        /*
-         * Setup the hwpe_mac accelerator in simple_mult mode.
-         * This computes the product A*B in 32 bits and stores it starting
-         * from the pointer given by O
-         * args:
-         *  a: pointer in TCDM (L1) to vector A
-         *  b: pointer in TCDM (L1) to vector B
-         *  o: pointer in TCDM (L1) to where output O must be stored
-         *  vector_length: length of A,B and O
-         * */
+    /* Setup the hwpe_mac accelerator in simple_mult mode.
+     * This computes the product A*B in 32 bits and stores it starting
+     * from the pointer given by O
+     * args:
+     *  a: pointer in TCDM (L1) to vector A
+     *  b: pointer in TCDM (L1) to vector B
+     *  o: pointer in TCDM (L1) to where output O must be stored
+     *  vector_length: length of A,B and O
+     * */
 
-        // Set addresses
-        write_csr(0x3d0, (uint32_t)a);
-        write_csr(0x3d1, (uint32_t)b);
-        write_csr(0x3d3, (uint32_t)o);
+    // Set addresses
+    write_csr(0x3d0, (uint32_t)a);
+    write_csr(0x3d1, (uint32_t)b);
+    write_csr(0x3d3, (uint32_t)o);
 
-        // Set configs
-        write_csr(0x3d4, 1);   // Number of iterations
-        write_csr(0x3d5, vector_length);  // Vector length
-        write_csr(0x3d6, 1);   // Set simple multiplication
+    // Set configs
+    write_csr(0x3d4, 1);   // Number of iterations
+    write_csr(0x3d5, vector_length);  // Vector length
+    write_csr(0x3d6, 1);   // Set simple multiplication
+}
+
+
+void cpu_simple_mult(uint32_t* a, uint32_t* b,
+                     uint32_t* o, uint32_t vector_length){
+    for (uint32_t i = 0; i < vector_length; i++) {
+        o[i] = a[i] * b[i];
+    };
+}
+
+int check_simple_mult(uint32_t* output, uint32_t* output_golden,
+                      uint32_t vector_length){
+    /*
+     * Compare output to output_golden with length vector_length
+     */
+    uint32_t err = 0;
+    for (uint32_t i = 0; i < vector_length; i++) {
+        // Check if output is same as golden output
+        if (output[i] != output_golden[i]) {
+            err++;
+        };
+    };
+    return err;
 }
 
 int main() {
-    // Set err value for checking
-    int err = 0;
 
     uint32_t *local_a, *local_b;
     uint32_t *local_o;
@@ -74,36 +94,27 @@ int main() {
 
         snax_mac_setup_simple_mult(local_a, local_b, local_o, VEC_LEN);
 
-        // Write start CSR to launch accelerator
-        write_csr(0x3c0, 0);
-
         // Start of CSR start 
         uint32_t mac_start = snrt_mcycle();
         
+        snax_mac_launch();
+
         // Wait until accelerator finishes
         snax_mac_sw_barrier();
 
         uint32_t mac_end = snrt_mcycle();
-        uint32_t cpu_checker;
 
-        for (uint32_t i = 0; i < (uint32_t)VEC_LEN; i++) {
-            // Check if output is same as golden output
-            if (*(local_o + i) != OUT[i]) {
-                err++;
-            };
+    }
 
-            // Compute using CPU multiplier
-            // Not the MAC output
-            cpu_checker = (*(local_a + i)) * (*(local_b + i));
-
-            // Compare if MAC output is same as CPU multiplier
-            if (*(local_o + i) != cpu_checker) {
-                err++;
-            };
-        };
-
-        uint32_t end_of_check = snrt_mcycle();
+    // Perform correctness check
+    int err = 0;
+    if (snrt_is_compute_core()){
+        err = check_simple_mult(local_o, OUT, VEC_LEN);
+        // Compute using CPU multiplier and check
+        uint32_t cpu_output[VEC_LEN];
+        cpu_simple_mult(local_a, local_b, cpu_output, VEC_LEN);
+        // Compare CPU result with golden model
+        err += check_simple_mult(cpu_output, OUT, VEC_LEN);
     };
-
     return err;
 }
