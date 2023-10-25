84d163eb73a1a0787c0466c3f1f2bee66d3adbbb
Josse Van Delm <josse.vandelm@gmail.com>
2023-10-25 16:20:18 +0200

    Add data generation for both simple vec examples (#36)
    * sw: Move files into snax-mac-simple folder
    
    * sw: Fix relative paths for CI after move
    
    * sw: Rename files for move
    
    * sw: Adapt relative paths for compilation
    
    * sw: Rename snax-mac-simple to untiled
    
    * sw: Use datagen to drive untiled example
    
    * sw: Fix C linting

--- target/snitch_cluster/sw/apps/snax-mac-simple/untiled/src/untiled.c
+++ target/snitch_cluster/sw/apps/snax-mac-simple/untiled/src/untiled.c
@@ -44,9 +44,9 @@ int main() {
         write_csr(0x3d3, (uint32_t)local_o);
 
         // Set configs
-        write_csr(0x3d4, 1);   // Number of iterations
-        write_csr(0x3d5, 19);  // Vector length
-        write_csr(0x3d6, 1);   // Set simple multiplication
+        write_csr(0x3d4, 1);        // Number of iterations
+        write_csr(0x3d5, VEC_LEN);  // Vector length
+        write_csr(0x3d6, 1);        // Set simple multiplication
 
         // Write start CSR to launch accelerator
         write_csr(0x3c0, 0);
