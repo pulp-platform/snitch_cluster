7a7b519420f07c060f2466caaa84246dc9077e0b
Luca Colagrande <luca.colagrande3@gmail.com>
2024-03-01 16:47:19 +0100

    Add missing license headers

--- target/snitch_cluster/util/snitch_cluster.py
+++ target/snitch_cluster/util/snitch_cluster.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# Copyright 2024 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Luca Colagrande <colluca@iis.ee.ethz.ch>
+
 from pathlib import Path
 import sys
 
