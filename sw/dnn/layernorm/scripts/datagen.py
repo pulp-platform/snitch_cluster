cfa32fbdc510a980496b79915247fc88622350e3
Viviane Potocnik <vivianep@iis.ee.ethz.ch>
2024-03-19 18:28:00 +0100

    dnn: use FlexFloat for data generation in LayerNorm kernel

--- sw/dnn/layernorm/scripts/datagen.py
+++ sw/dnn/layernorm/scripts/datagen.py
@@ -7,24 +7,21 @@
 # Viviane Potocnik <vivianep@iis.ee.ethz.ch>
 # Luca Colagrande <colluca@iis.ee.ethz.ch>
 
+from data_utils import emit_license, \
+    format_struct_definition, format_array_definition, \
+    format_array_declaration, format_ifdef_wrapper
 import argparse
-import array
-from bitstring import Array
 import pathlib
 import json5
 import sys
 import os
 import torch
-import tensorflow as tf
-from tensorflow.python.framework import dtypes
 import numpy as np
+
+import pyflexfloat as ff
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../../../util/sim/"))
 import data_utils  # noqa: E402
-from data_utils import emit_license, \
-                       format_struct_definition, format_array_definition, \
-                       format_array_declaration, format_ifdef_wrapper, \
-                       apply_to_tensor, NUMPY_TYPES, FP8_FORMATS  # noqa: E402
 
 torch.manual_seed(42)
 
@@ -32,11 +29,32 @@ torch.manual_seed(42)
 # the occurrence of these splits the data should be aligned to 4KB
 BURST_ALIGNMENT = 4096
 
-def golden_model(ifmap, eps, shape):
+
+def golden_model(ifmap, eps):
+    # Compute the mean and variance considering the last dimension (embeddings)
+    # The dimensions for mean and variance calculations are (-1,), meaning the last dimension.
+    # Keep the dimensions for broadcasting in normalization.
+    mean = np.mean(ifmap, axis=(-1,), keepdims=True)
+    diff = ifmap - mean
+    var = np.mean(diff*diff, axis=(-1,), keepdims=True)
+
+    # Normalize the input tensor
+    ofmap = (ifmap - mean) / np.sqrt(var + eps)
+
+    return ofmap
+
+
+def golden_model_torch(ifmap, eps, shape):
     ln = torch.nn.LayerNorm(shape, eps=eps)
     return ln(ifmap)
 
+
+def validate_config(seq_len, n_tiles, **kwargs):
+    assert (seq_len % n_tiles) == 0, 'Input dimension is not an integer multiple of tile size'
+
+
 def emit_header(**kwargs):
+
     batch_size = kwargs['input_dim']['batch_size']
     seq_len = kwargs['input_dim']['seq_len']
     embeddings = kwargs['input_dim']['embeddings']
@@ -44,42 +62,16 @@ def emit_header(**kwargs):
     prec = kwargs['prec']
     n_tiles = kwargs['n_tiles']
     baseline = kwargs['baseline']
-    dtype = NUMPY_TYPES[str(prec)]
 
-    assert (seq_len % n_tiles) == 0, 'Input dimension is not an integer multiple of tile size'
-    
-    torch_type = data_utils.torch_type_from_precision_t(prec)
-    # TODO: Add flag for FP8 and FP8ALT precision
-    if prec == "FP8" or prec == "FP8ALT":
-        if prec == "FP8":
-            fp8_var = torch.float8_e5m2
-        else:
-            fp8_var = torch.float8_e4m3fn
-            
-        fmt = prec.lower()
-        exp = FP8_FORMATS[fmt]['exp']
-        mant = FP8_FORMATS[fmt]['mant']
+    # Validate parameters
+    validate_config(seq_len, **kwargs)
 
-
-        ifmap_rand = np.random.rand(batch_size, seq_len, embeddings).astype(np.float32)
-        ifmap_float8 = torch.tensor(ifmap_rand).to(fp8_var)
-        ifmap_float32 = ifmap_float8.to(torch.float32)
-        ofmap_layernorm = golden_model(ifmap_float32, eps, embeddings)
-        # we have to downcast the output to FP8
-        ofmap_float8 = ofmap_layernorm.to(fp8_var)
-        ofmap_float32 = ofmap_float8.to(torch.float32)
-        ifmap = apply_to_tensor(ifmap_float32, exp, mant)
-        ofmap = apply_to_tensor(ofmap_float32, exp, mant)
-    else:
-        # FIXME: 16-bit precision is not supported by torch.nn.LayerNorm
-        if torch_type != torch.float32:
-            torch_type = torch.float32
-        ifmap = torch.randn(batch_size, seq_len, embeddings, requires_grad=False, dtype=torch_type)
-
-        ofmap = golden_model(ifmap, eps, embeddings)
-        ofmap = ofmap.detach().numpy()
-
+    ff_desc = data_utils.ff_desc_from_precision_t(prec)
     ctype = data_utils.ctype_from_precision_t(prec)
+
+    # Generate random input
+    ifmap = ff.array(np.random.rand(batch_size, seq_len, embeddings), ff_desc)
+    ofmap = golden_model(ifmap, eps)
 
     ifmap_uid = 'ifmap'
     ofmap_uid = 'ofmap'
