fa5c1948099f70a0235d8961c8df257664fd2327
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2024-04-26 09:23:37 +0200

    make compatible with verilator 5 (#97)
    * add timing flag
    
    * extra flags
    
    * set c++20 standard
    
    * set c++20
    
    * maintain reverse compatibility
    
    * set cflags
    
    * undo unwanted comment
    
    * bug

--- target/common/common.mk
+++ target/common/common.mk
@@ -33,6 +33,7 @@ EVENTVIS_PY      ?= $(UTIL_DIR)/trace/eventvis.py
 
 VERILATOR_ROOT ?= $(dir $(shell $(VERILATOR_SEPP) which verilator))..
 VLT_ROOT       ?= ${VERILATOR_ROOT}
+VERILATOR_VERSION ?= $(shell $(VLT) --version | grep -oP 'Verilator \K\d+')
 
 MATCH_END := '/+incdir+/ s/$$/\/*\/*/'
 MATCH_BGN := 's/+incdir+//g'
@@ -57,6 +58,9 @@ VLT_BENDER   += -t rtl
 VLT_SOURCES   = $(shell ${BENDER} script flist ${VLT_BENDER} | ${SED_SRCS})
 VLT_BUILDDIR := work-vlt
 VLT_FESVR     = $(VLT_BUILDDIR)/riscv-isa-sim
+ifeq ($(VERILATOR_VERSION), 5)
+	VLT_FLAGS += --timing
+endif
 VLT_FLAGS    += -Wno-BLKANDNBLK
 VLT_FLAGS    += -Wno-LITENDIAN
 VLT_FLAGS    += -Wno-CASEINCOMPLETE
@@ -68,7 +72,11 @@ VLT_FLAGS    += -Wno-UNOPTFLAT
 VLT_FLAGS    += -Wno-fatal
 VLT_FLAGS    += +define+SYNTHESIS
 VLT_FLAGS    += --unroll-count 1024
-VLT_CFLAGS   += -std=c++14 -pthread
+ifeq ($(VERILATOR_VERSION), 5)
+	VLT_CFLAGS += -std=c++20 -pthread
+else 
+	VLT_CFLAGS += -std=c++14 -pthread
+endif
 VLT_CFLAGS   +=-I ${VLT_BUILDDIR} -I $(VLT_ROOT)/include -I $(VLT_ROOT)/include/vltstd -I $(VLT_FESVR)/include -I $(TB_DIR) -I ${MKFILE_DIR}/test
 
 ANNOTATE_FLAGS ?= -q --keep-time
