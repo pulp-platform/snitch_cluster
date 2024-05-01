9117bff1afb4b6ee2a7fe3695abb8d050e93bfde
Markus Böck <markus.boeck02@gmail.com>
2024-05-01 21:42:55 +0100

    sw: Add missing `extern` declarations
    Without these `extern` declarations, the final object file will not contain functions marked `inline` as LLVM discards them prior to code generation.
    
    This has so far not been problematic as these methods usually get inlined. Downstream projects as well as consumers of the `api/` headers may, however, be using different compiler flags or not have inline definitions available within the same translation unit. This would then cause an "undefined reference" linker error in any such project.

--- sw/snRuntime/src/alloc.c
+++ sw/snRuntime/src/alloc.c
@@ -4,11 +4,19 @@
 
 snrt_allocator_t l3_allocator;
 
+extern volatile uint32_t *snrt_zero_memory_ptr();
+
 extern void *snrt_l1_next();
 extern void *snrt_l3_next();
 
+extern uint32_t snrt_l1_start_addr();
+extern uint32_t snrt_l1_end_addr();
+
 extern void *snrt_l1alloc(size_t size);
 extern void *snrt_l3alloc(size_t size);
+
+extern snrt_allocator_t *snrt_l1_allocator();
+extern snrt_allocator_t *snrt_l3_allocator();
 
 extern void snrt_l1_update_next(void *next);
 
