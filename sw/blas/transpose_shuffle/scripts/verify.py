f727a63bb580baa3f070c1c094838108819fe234
sem24f2 Lucia Luzi (luzil) <sem24f2@badile23.ee.ethz.ch>
2024-05-27 21:21:20 +0200

    kernel done for fp32, fp16, fp8

--- sw/blas/transpose_shuffle/scripts/verify.py
+++ sw/blas/transpose_shuffle/scripts/verify.py
@@ -17,32 +17,26 @@ from data_utils import ctype_from_precision_t  # noqa: E402
 class TransposeVerifier(Verifier):
 
     OUTPUT_UIDS = ['output']
+    ERR_THRESHOLD = {8: 1e-6, 4: 1e-6, 2: 1e-2, 1: 1e-4}
 
     def __init__(self):
         super().__init__()
-        self.layer_struct = {
-            'M': 'I',
-            'N': 'I',
-            'input_ptr': 'I',
-            'output_ptr': 'I',
-            'dtype': 'I',
-            'baseline': 'I'
-        }
-        self.layer = self.get_input_from_symbol('layer', self.layer_struct)
-        self.M = self.layer['M']
-        self.N = self.layer['N']
-        self.prec = self.layer['dtype']
+
+        self.M = self.get_input_from_symbol('M', 'uint32_t')[0]
+        self.N = self.get_input_from_symbol('N', 'uint32_t')[0]
+        self.prec = self.get_input_from_symbol('dtype', 'uint32_t')[0]
 
     def get_actual_results(self):
-        return self.get_output_from_symbol('output', ctype_from_precision_t(self.prec))
+        return self.get_output_from_symbol('output', ctype_from_precision_t(self.prec)).reshape(self.N, self.M)
 
     def get_expected_results(self):
         inp = self.get_input_from_symbol('input', ctype_from_precision_t(self.prec))
         inp = inp.reshape(self.M, self.N)
         return TransposeDataGen().golden_model(inp)
+        # return self.get_input_from_symbol('golden', ctype_from_precision_t(self.prec))
 
     def check_results(self, *args):
-        return super().check_results(*args, atol=0)
+        return super().check_results(*args, rtol=self.ERR_THRESHOLD[self.prec])
 
 
 if __name__ == "__main__":
