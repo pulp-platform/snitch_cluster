7462d50ddc88c3ebafe296554a9628383e15c8a0
Luca Colagrande <luca.colagrande3@gmail.com>
2024-07-03 21:56:02 +0200

    Extend on Vivi's refactoring of the `run.py` scripts

--- target/snitch_cluster/util/build.py
+++ target/snitch_cluster/util/build.py
@@ -5,27 +5,58 @@
 #
 # Viviane Potocnik <vivianep@iis.ee.ethz.ch>
 # Luca Colagrande <colluca@iis.ee.ethz.ch>
+"""Utility to build a software app using multiple configuration files.
 
+This utility builds multiple executables from the same application code,
+provided with different data.
+
+It assumes a build system with a specific interface which most existing
+kernels comply with. In particular, it assumes the following:
+
+- the data generation can be configured through a YAML file. The path to
+this file is provided to the build system in the `DATA_CFG` environment
+variable.
+- the generated data is written to a header file at the path specified by
+the `DATA_H` environment variable.
+- all build artifacts are created in the directory specified by the
+`APP_BUILDDIR` environment variable.
+- the build command should be a make target defined by the Snitch cluster
+target.
+
+This utility takes a list of data configuration files as input and builds
+the application with the data generated from each configuration. Build
+artifacts are stored in a separate directory for each configuration.
+
+The utility can optionally generate a testlist file which can be used to
+run the generated executables using the [`run.py`][run] utility.
+"""
+
+import argparse
 import os
 from pathlib import Path
 import subprocess
-import sys
 import yaml
 
-import snitch_cluster  # noqa: E402
 
 def parser():
     # Argument parsing
-    parser = snitch_cluster.parser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'target',
+        help='Make target to use for the build')
     parser.add_argument(
         '--cfg',
         nargs='*',
+        required=True,
         help='List of configurations')
+    parser.add_argument(
+        '--testlist',
+        help='Output a testlist with the generated executables to this file')
+    parser.add_argument(
+        '--testlist-cmd',
+        help='A custom command to use in the testlist')
+
     return parser
-
-
-def parse_args():
-    return parser().parse_args()
 
 
 def extend_environment(env=None, **kwargs):
@@ -34,71 +65,56 @@ def extend_environment(env=None, **kwargs):
     env.update(kwargs)
     return env
 
-# Build software for a specific configuration
-def build_sw(cfg):
+
+# Build software target with a specific data configuration
+def build(target, cfg):
+    # Define configuration-specific variables for build system
     env = extend_environment(
         DATA_CFG=cfg,
         DATA_H=Path(f'include/{cfg.stem}/data.h').resolve(),
-        APP_DIR=Path(f'{cfg.parents[1]}').resolve(),
         APP_BUILDDIR=Path(f'build/{cfg.stem}').resolve()
     )
-    app_dir = str(env['APP_DIR'])
-    cut_point = "sw/"
-    index = app_dir.find(cut_point)
-    # Slice the string after "sw/"
-    if index != -1:
-        result = app_dir[index:]
-    else:
-        result = app_dir
-    print(f'APP_DIR: {result}')
-    print(f'CWD {Path.cwd()}')
-    # subprocess.run(['make', '-C', '../../../../../', 'DEBUG=ON', 'sw/apps/blas/gemm'],
-    #                check=True, env=env)
-    print(f'make -C ../../../../ DEBUG=ON {str(result)}')
-    subprocess.run(['make', '-C', '../../../../', 'DEBUG=ON', str(result)],
+
+    # Build the software
+    mk_dir = Path(__file__).resolve().parent.parent
+    subprocess.run(['make', '-C', mk_dir, 'DEBUG=ON', target],
                    check=True, env=env)
 
 
-# Build test specification for a specific configuration
-def build_test(cfg):
-    return {
-        'elf': f'build/{cfg.stem}/gemm.elf',
-        'name': f'gemm-{cfg.stem}',
-        'cmd': [
-            '../../../../../../../sw/blas/gemm/scripts/verify.py',
-            "${sim_bin}",
-            "${elf}",
-            '--dump-results'
-        ]
+# Create test specification for a specific configuration
+def create_test(target, cfg, cmd=None):
+    cmd_list = cmd.split()
+    cfg = cfg.stem
+    app = Path(target).stem
+    test = {
+        'elf': f'build/{cfg}/{app}.elf',
+        'name': f'{app}-{cfg}',
     }
+    if cmd:
+        test['cmd'] = cmd_list
+    return test
 
 
 # Build testlist file
-def build_testlist(tests, outfile):
-    with open(outfile, 'w') as f:
-        yaml.dump({'runs': tests}, f)
+def dump_testlist(tests, outfile):
+    if outfile:
+        with open(outfile, 'w') as f:
+            yaml.dump({'runs': tests}, f)
 
 
 def main():
 
-    args = parse_args()
+    # Parse arguments
+    args = parser().parse_args()
+    cfgs = [Path(cfg) for cfg in args.cfg]
 
-    print(f'Arguments: {args}')
+    # Build software
+    for cfg in cfgs:
+        build(args.target, cfg)
 
-    # Build software and test specifications
-    tests = []
-    for cfg in args.cfg:
-        print(f'Building SW for cfg: {cfg}')
-        cfg = Path(cfg)
-        build_sw(cfg)
-        # tests.append(build_test(cfg))
-
-    # # Build the testlist
-    # build_testlist(tests, args.testlist)
-
-    # # Run the simulations
-    # simulations = snitch_cluster.get_simulations(args)
-    # snitch_cluster.run_simulations(simulations, args)
+    # Build testlist
+    tests = [create_test(args.target, cfg, args.testlist_cmd) for cfg in cfgs]
+    dump_testlist(tests, args.testlist)
 
 
 if __name__ == '__main__':
