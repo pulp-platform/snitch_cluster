ea0d502525dede89d28532a005a6558209bbfc88
Tim Fischer <fischeti@iis.ee.ethz.ch>
2024-07-16 15:18:14 +0200

    sw: Refactor performance counter runtime functions

--- sw/snRuntime/src/perf_cnt.h
+++ sw/snRuntime/src/perf_cnt.h
@@ -2,100 +2,53 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#pragma once
+#define SNRT_NUM_PERF_CNTS SNITCH_CLUSTER_PERIPHERAL_PARAM_NUM_PERF_COUNTERS
 
-/// Different perf counters
-// Must match with `snitch_cluster_peripheral`
-enum snrt_perf_cnt {
-    SNRT_PERF_CNT0,
-    SNRT_PERF_CNT1,
-    SNRT_PERF_CNT2,
-    SNRT_PERF_CNT3,
-    SNRT_PERF_CNT4,
-    SNRT_PERF_CNT5,
-    SNRT_PERF_CNT6,
-    SNRT_PERF_CNT7,
-    SNRT_PERF_CNT8,
-    SNRT_PERF_CNT9,
-    SNRT_PERF_CNT10,
-    SNRT_PERF_CNT11,
-    SNRT_PERF_CNT12,
-    SNRT_PERF_CNT13,
-    SNRT_PERF_CNT14,
-    SNRT_PERF_CNT15,
-    SNRT_PERF_N_CNT,
-};
-
-/// Different types of performance counters
-enum snrt_perf_cnt_type {
-    SNRT_PERF_CNT_CYCLES,
-    SNRT_PERF_CNT_TCDM_ACCESSED,
-    SNRT_PERF_CNT_TCDM_CONGESTED,
-    SNRT_PERF_CNT_ISSUE_FPU,
-    SNRT_PERF_CNT_ISSUE_FPU_SEQ,
-    SNRT_PERF_CNT_ISSUE_CORE_TO_FPU,
-    SNRT_PERF_CNT_RETIRED_INSTR,
-    SNRT_PERF_CNT_RETIRED_LOAD,
-    SNRT_PERF_CNT_RETIRED_I,
-    SNRT_PERF_CNT_RETIRED_ACC,
-    SNRT_PERF_CNT_DMA_AW_STALL,
-    SNRT_PERF_CNT_DMA_AR_STALL,
-    SNRT_PERF_CNT_DMA_R_STALL,
-    SNRT_PERF_CNT_DMA_W_STALL,
-    SNRT_PERF_CNT_DMA_BUF_W_STALL,
-    SNRT_PERF_CNT_DMA_BUF_R_STALL,
-    SNRT_PERF_CNT_DMA_AW_DONE,
-    SNRT_PERF_CNT_DMA_AW_BW,
-    SNRT_PERF_CNT_DMA_AR_DONE,
-    SNRT_PERF_CNT_DMA_AR_BW,
-    SNRT_PERF_CNT_DMA_R_DONE,
-    SNRT_PERF_CNT_DMA_R_BW,
-    SNRT_PERF_CNT_DMA_W_DONE,
-    SNRT_PERF_CNT_DMA_W_BW,
-    SNRT_PERF_CNT_DMA_B_DONE,
-    SNRT_PERF_CNT_DMA_BUSY,
-    SNRT_PERF_CNT_ICACHE_MISS,
-    SNRT_PERF_CNT_ICACHE_HIT,
-    SNRT_PERF_CNT_ICACHE_PREFETCH,
-    SNRT_PERF_CNT_ICACHE_DOUBLE_HIT,
-    SNRT_PERF_CNT_ICACHE_STALL,
-};
+typedef struct {
+    uint16_t hart;
+    uint16_t metric;
+} perf_cnt_cfg_t;
 
 typedef union {
     uint32_t value __attribute__((aligned(8)));
+    perf_cnt_cfg_t cfg __attribute__((aligned(8)));
 } perf_reg32_t;
 
 typedef struct {
-    volatile perf_reg32_t enable[SNRT_PERF_N_CNT];
-    volatile perf_reg32_t hart_select[SNRT_PERF_N_CNT];
-    volatile perf_reg32_t perf_counter[SNRT_PERF_N_CNT];
+    volatile perf_reg32_t enable[SNRT_NUM_PERF_CNTS];
+    volatile perf_reg32_t select[SNRT_NUM_PERF_CNTS];
+    volatile perf_reg32_t perf_counter[SNRT_NUM_PERF_CNTS];
 } perf_regs_t;
 
+// Return the pointer to the perf_counters
 inline perf_regs_t* snrt_perf_counters() {
     return (perf_regs_t*)snrt_cluster_perf_counters_addr();
 }
 
+// Configure a specific perf_counter
+inline void snrt_cfg_perf_counter(uint32_t perf_cnt, uint16_t metric, uint16_t hart) {
+    // Make sure the configuration is written in a single write
+    perf_reg32_t cfg_reg;
+    cfg_reg.cfg = (perf_cnt_cfg_t){.metric = metric, .hart = hart};
+    snrt_perf_counters()->select[perf_cnt].value = cfg_reg.value;
+}
+
 // Enable a specific perf_counter
-inline void snrt_start_perf_counter(enum snrt_perf_cnt perf_cnt,
-                                    enum snrt_perf_cnt_type perf_cnt_type,
-                                    uint32_t hart_id) {
-    snrt_perf_counters()->hart_select[perf_cnt].value |= hart_id;
-    snrt_perf_counters()->enable[perf_cnt].value = (0x1 << perf_cnt_type);
+inline void snrt_start_perf_counter(uint32_t perf_cnt) {
+    snrt_perf_counters()->enable[perf_cnt].value = 0x1;
 }
 
 // Stops the counter but does not reset it
-inline void snrt_stop_perf_counter(enum snrt_perf_cnt perf_cnt) {
+inline void snrt_stop_perf_counter(uint32_t perf_cnt) {
     snrt_perf_counters()->enable[perf_cnt].value = 0x0;
 }
 
 // Resets the counter completely
-inline void snrt_reset_perf_counter(enum snrt_perf_cnt perf_cnt) {
-    snrt_perf_counters()->enable[perf_cnt].value = 0x0;
-    snrt_perf_counters()->hart_select[perf_cnt].value = 0x0;
+inline void snrt_reset_perf_counter(uint32_t perf_cnt) {
     snrt_perf_counters()->perf_counter[perf_cnt].value = 0x0;
 }
 
 // Get counter of specified perf_counter
-inline uint32_t snrt_get_perf_counter(enum snrt_perf_cnt perf_cnt) {
-    return (uint32_t)(snrt_perf_counters()->perf_counter[perf_cnt].value);
+inline uint32_t snrt_get_perf_counter(uint32_t perf_cnt) {
+    return snrt_perf_counters()->perf_counter[perf_cnt].value;
 }
