877a237c8646530ccfad9fedd313597ecc9af14e
Tim Fischer <fischeti@iis.ee.ethz.ch>
2024-07-22 16:35:08 +0200

    test: Use simple bootrom that directly jumps to L3

--- target/snitch_cluster/test/snitch_bootrom.sv
+++ target/snitch_cluster/test/snitch_bootrom.sv
@@ -32,24 +32,11 @@ module snitch_bootrom #(
 
     always_comb begin : gen_bootrom
         bootrom = '0;
-            bootrom[0] = 32'h00000297; /* 0x0000 */
-            bootrom[1] = 32'h02c28293; /* 0x0004 */
-            bootrom[2] = 32'h30529073; /* 0x0008 */
-            bootrom[3] = 32'h30046073; /* 0x000c */
-            bootrom[4] = 32'h000802b7; /* 0x0010 */
-            bootrom[5] = 32'h00828293; /* 0x0014 */
-            bootrom[6] = 32'h00100313; /* 0x0018 */
-            bootrom[7] = 32'h00b31313; /* 0x001c */
-            bootrom[8] = 32'h0062e2b3; /* 0x0020 */
-            bootrom[9] = 32'h30429073; /* 0x0024 */
-            bootrom[10] = 32'h10500073; /* 0x0028 */
-            bootrom[11] = 32'h00000297; /* 0x002c */
-            bootrom[12] = 32'hfffff337; /* 0x0030 */
-            bootrom[13] = 32'h15c30313; /* 0x0034 */
-            bootrom[14] = 32'h006282b3; /* 0x0038 */
-            bootrom[15] = 32'h0002a283; /* 0x003c */
-            bootrom[16] = 32'h000280e7; /* 0x0040 */
-            bootrom[17] = 32'hfbdff06f; /* 0x0044 */
+            bootrom[0] = 32'h800002b7; /* 0x0000 */
+            bootrom[1] = 32'h00028293; /* 0x0004 */
+            bootrom[2] = 32'h00028067; /* 0x0008 */
+            bootrom[3] = 32'h10500073; /* 0x000c */
+            bootrom[4] = 32'hffdff06f; /* 0x0010 */
     end
 
 endmodule
