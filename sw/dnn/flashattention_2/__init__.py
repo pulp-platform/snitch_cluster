b31207071436e1e102c421b78f982d4a24847c46
Viviane Potocnik <vivianep@iis.ee.ethz.ch>
2024-08-08 15:32:04 +0200

    [treewide] formatting: align all Python sources and fix import errors on init scripts

--- sw/dnn/flashattention_2/__init__.py
+++ sw/dnn/flashattention_2/__init__.py
@@ -3,5 +3,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Luca Colagrande <colluca@iis.ee.ethz.ch>
+# Viviane Potocnik <vivianep@iis.ee.ethz.ch>
 
-from .scripts.datagen import *
+from .scripts.datagen import exact_golden_model, exact_flexfloat_golden_model, \
+                             get_gemm_implementation, load_params, \
+                             validate, emit_header
+
+__all__ = ['exact_golden_model', 'exact_flexfloat_golden_model',
+           'get_gemm_implementation', 'load_params', 'validate', 'emit_header']
