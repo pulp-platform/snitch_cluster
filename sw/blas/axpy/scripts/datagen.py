6d6a94af3fef8b5704e878de14577495e5fc6fb1
Luca Colagrande <luca.colagrande3@gmail.com>
2024-08-12 10:35:40 +0200

    sw/blas/axpy: Add multiple impls and optimize TCDM placement

--- sw/blas/axpy/scripts/datagen.py
+++ sw/blas/axpy/scripts/datagen.py
@@ -9,7 +9,7 @@ import numpy as np
 import sys
 
 from snitch.util.sim.data_utils import format_scalar_definition, format_array_definition, \
-    format_array_declaration, format_ifdef_wrapper, DataGen
+    format_array_declaration, format_ifdef_wrapper, format_struct_definition, DataGen
 
 
 class AxpyDataGen(DataGen):
@@ -19,29 +19,53 @@ class AxpyDataGen(DataGen):
     # AXI splits bursts crossing 4KB address boundaries. To minimize
     # the occurrence of these splits the data should be aligned to 4KB
     BURST_ALIGNMENT = 4096
+    # Function pointers to alternative implementations
+    FUNCPTRS = ["axpy_naive", "axpy_fma", "axpy_opt"]
 
     def golden_model(self, a, x, y):
         return a*x + y
 
+    def validate_config(self, **kwargs):
+        assert (kwargs['n'] % 8) == 0, "n must be an integer multiple of the number of cores"
+        assert kwargs['funcptr'] in self.FUNCPTRS, f"Function pointer must be among {self.FUNCPTRS}"
+
+        # Calculate total TCDM occupation
+        # Note: doesn't account for double buffering
+        vec_size = kwargs['n'] * 8
+        total_size = 3 * vec_size
+        data_utils.validate_tcdm_footprint(total_size)
+
     def emit_header(self, **kwargs):
         header = [super().emit_header()]
 
-        n = kwargs['n']
-        a = np.random.uniform(self.MIN, self.MAX, 1)
-        x = np.random.uniform(self.MIN, self.MAX, n)
-        y = np.random.uniform(self.MIN, self.MAX, n)
+        self.validate_config(**kwargs)
+
+        a = np.random.uniform(self.MIN, self.MAX, 1)[0]
+        x = np.random.uniform(self.MIN, self.MAX, kwargs['n'])
+        y = np.random.uniform(self.MIN, self.MAX, kwargs['n'])
         g = self.golden_model(a, x, y)
 
-        assert (n % 8) == 0, "n must be an integer multiple of the number of cores"
+        x_uid = 'x'
+        y_uid = 'y'
+        z_uid = 'z'
 
-        header += [format_scalar_definition('const uint32_t', 'n', n)]
-        header += [format_scalar_definition('const double', 'a', a[0])]
-        header += [format_array_definition('double', 'x', x, alignment=self.BURST_ALIGNMENT,
-                                           section=kwargs['section'])]
-        header += [format_array_definition('double', 'y', y, alignment=self.BURST_ALIGNMENT,
-                                           section=kwargs['section'])]
-        header += [format_array_declaration('double', 'z', [n], alignment=self.BURST_ALIGNMENT,
-                                            section=kwargs['section'])]
+        cfg = {
+            'n': kwargs['n'],
+            'a': a,
+            'x': x_uid,
+            'y': y_uid,
+            'z': z_uid,
+            'funcptr': kwargs['funcptr']
+        }
+
+        header += [format_scalar_definition('const double', 'a', a)]
+        header += [format_array_definition('double', x_uid, x,
+                   alignment=self.BURST_ALIGNMENT, section=kwargs['section'])]
+        header += [format_array_definition('double', y_uid, y,
+                   alignment=self.BURST_ALIGNMENT, section=kwargs['section'])]
+        header += [format_array_declaration('double', z_uid, x.shape,
+                   alignment=self.BURST_ALIGNMENT, section=kwargs['section'])]
+        header += [format_struct_definition('axpy_args_t', 'args', cfg)]
         result_def = format_array_definition('double', 'g', g)
         header += [format_ifdef_wrapper('BIST', result_def)]
         header = '\n\n'.join(header)
--- sw/blas/axpy/src/axpy.h
+++ sw/blas/axpy/src/axpy.h
@@ -2,28 +2,47 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#include "args.h"
 #include "snrt.h"
 
-inline void axpy(uint32_t n, double a, double* x, double* y, double* z) {
+#define BANK_ALIGNMENT 8
+#define TCDM_ALIGNMENT (32 * BANK_ALIGNMENT)
+#define ALIGN_UP_TCDM(addr) ALIGN_UP(addr, TCDM_ALIGNMENT)
+
+static inline void axpy_naive(uint32_t n, double a, double* x, double* y, double* z) {
     int core_idx = snrt_cluster_core_idx();
     int frac = n / snrt_cluster_compute_core_num();
-    int offset = core_idx * frac;
+    int offset = core_idx;
 
-#ifndef XSSR
-
-    for (int i = 0; i < frac; i++) {
-        z[offset] = a * x[offset] + y[offset];
-        offset++;
+    for (int i = offset; i < n; i += snrt_cluster_compute_core_num()) {
+        z[i] = a * x[i] + y[i];
     }
     snrt_fpu_fence();
+}
 
-#else
+static inline void axpy_fma(uint32_t n, double a, double* x, double* y, double* z) {
+    int core_idx = snrt_cluster_core_idx();
+    int frac = n / snrt_cluster_compute_core_num();
+    int offset = core_idx;
 
-    // TODO(colluca): revert once Banshee supports SNRT_SSR_DM_ALL
-    // snrt_ssr_loop_1d(SNRT_SSR_DM_ALL, frac, sizeof(double));
-    snrt_ssr_loop_1d(SNRT_SSR_DM0, frac, sizeof(double));
-    snrt_ssr_loop_1d(SNRT_SSR_DM1, frac, sizeof(double));
-    snrt_ssr_loop_1d(SNRT_SSR_DM2, frac, sizeof(double));
+    for (int i = offset; i < n; i += snrt_cluster_compute_core_num()) {
+        asm volatile (
+            "fmadd.d %[z], %[a], %[x], %[y] \n"
+            : [ z ]"=f"(z[i])
+            : [ a ]"f"(a), [ x ]"f"(x[i]), [ y ]"f"(y[i])
+        );
+    }
+    snrt_fpu_fence();
+}
+
+static inline void axpy_opt(uint32_t n, double a, double* x, double* y, double* z) {
+    int core_idx = snrt_cluster_core_idx();
+    int frac = n / snrt_cluster_compute_core_num();
+    int offset = core_idx;
+
+    snrt_ssr_loop_1d(SNRT_SSR_DM_ALL,
+                     frac,
+                     snrt_cluster_compute_core_num() * sizeof(double));
 
     snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_1D, x + offset);
     snrt_ssr_read(SNRT_SSR_DM1, SNRT_SSR_1D, y + offset);
@@ -36,10 +55,70 @@ inline void axpy(uint32_t n, double a, double* x, double* y, double* z) {
         "fmadd.d ft2, %[a], ft0, ft1\n"
         :
         : [ n_frep ] "r"(frac - 1), [ a ] "f"(a)
-        : "ft0", "ft1", "ft2", "memory");
-
+        : "ft0", "ft1", "ft2", "memory"
+    );
+    
     snrt_fpu_fence();
     snrt_ssr_disable();
+}
 
+static inline void axpy_job(axpy_args_t *args) {
+    uint64_t local_x_addr, local_y_addr, local_z_addr;
+    double *local_x, *local_y, *local_z;
+    double *remote_x, *remote_y, *remote_z;
+
+#ifndef JOB_ARGS_PRELOADED
+    // Allocate space for job arguments in TCDM
+    axpy_args_t *local_args = (axpy_args_t *)snrt_l1_next();
+
+    // Copy job arguments to TCDM
+    if (snrt_is_dm_core()) {
+        snrt_dma_start_1d(local_args, args, sizeof(axpy_args_t));
+        snrt_dma_wait_all();
+    }
+    snrt_cluster_hw_barrier();
+    args = local_args;
 #endif
+
+    // Calculate size and pointers for each cluster
+    uint32_t frac = args->n / snrt_cluster_num();
+    uint32_t offset = frac * snrt_cluster_idx();
+    remote_x = args->x + offset;
+    remote_y = args->y + offset;
+    remote_z = args->z + offset;
+
+    // Allocate space for job operands in TCDM
+    // Align X with the 1st bank in TCDM, Y with the 8th and Z with the 16th.
+    local_x_addr = ALIGN_UP_TCDM((uint64_t)args + sizeof(axpy_args_t));
+    local_y_addr = ALIGN_UP_TCDM(local_x_addr + frac * sizeof(double)) + 8 * BANK_ALIGNMENT;
+    local_z_addr = ALIGN_UP_TCDM(local_y_addr + frac * sizeof(double)) + 16 * BANK_ALIGNMENT;
+    local_x = (double *)local_x_addr;
+    local_y = (double *)local_y_addr;
+    local_z = (double *)local_z_addr;
+
+    // Copy job operands in TCDM
+    if (snrt_is_dm_core()) {
+        size_t size = frac * sizeof(double);
+        snrt_dma_start_1d(local_x, remote_x, size);
+        snrt_dma_start_1d(local_y, remote_y, size);
+        snrt_dma_wait_all();
+    }
+    snrt_cluster_hw_barrier();
+
+    // Compute
+    if (!snrt_is_dm_core()) {
+        axpy_fp_t fp = args->funcptr;
+        uint32_t start_cycle = snrt_mcycle();
+        fp(frac, args->a, local_x, local_y, local_z);
+        uint32_t end_cycle = snrt_mcycle();
+    }
+    snrt_cluster_hw_barrier();
+
+    // Copy data out of TCDM
+    if (snrt_is_dm_core()) {
+        size_t size = frac * sizeof(double);
+        snrt_dma_start_1d(remote_z, local_z, size);
+        snrt_dma_wait_all();
+    }
+    snrt_cluster_hw_barrier();
 }
