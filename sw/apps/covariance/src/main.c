f63c6c9f41ee7fd02230880f3965d26dca4c1617
Luca Colagrande <luca.colagrande3@gmail.com>
2024-08-14 00:25:29 +0200

    sw: Add optimized covariance kernel

--- sw/apps/covariance/src/main.c
+++ sw/apps/covariance/src/main.c
@@ -1,56 +1,17 @@
-// Copyright 2023 ETH Zurich and University of Bologna.
+// Copyright 2024 ETH Zurich and University of Bologna.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 //
-// Author: Jose Pedro Castro Fonseca <jcastro@ethz.ch>
-//         Luca Colagrande <colluca@iis.ee.ethz.ch>
+// Author: Luca Colagrande <colluca@iis.ee.ethz>
+
+#include "snrt.h"
 
 #include "covariance.h"
 #include "data.h"
 
-#define MAX_ERROR 1e-10
-
 int main() {
-    uint32_t nerr = 0;
-    double *local_mean;
-    double *local_cov;
-    double *local_data;
-    double diff;
 
-    local_data = snrt_l1_next();
-    local_cov = local_data + N * M;
+    covariance_job(&args);
 
-    // Initialize input matrix
-    if (snrt_is_dm_core()) {
-        snrt_dma_start_1d(local_data, data, sizeof(double) * N * M);
-        snrt_dma_wait_all();
-    }
-    snrt_cluster_hw_barrier();
-
-    // Perform Computations
-    kernel_covariance(N, M, local_data, local_cov);
-    snrt_cluster_hw_barrier();
-
-    // Writeback outputs
-    if (snrt_is_dm_core()) {
-        snrt_dma_start_1d(cov, local_cov, sizeof(double) * M * M);
-        snrt_dma_wait_all();
-    }
-    snrt_cluster_hw_barrier();
-
-#ifdef BIST
-    // Check computation is correct
-    if (snrt_cluster_core_idx() == 0) {
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < M; j++) {
-                diff = fabs(golden[i * M + j] - local_cov[i * M + j]);
-                if (diff > MAX_ERROR) {
-                    nerr++;
-                }
-            }
-        }
-    }
-#endif
-
-    return nerr;
+    return 0;
 }
