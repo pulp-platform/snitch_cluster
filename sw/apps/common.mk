36e69b17cc207375a8737b8f1fdde55636877472
Luca Colagrande <luca.colagrande3@gmail.com>
2024-08-22 15:23:01 +0200

    sw: Enable passing per-app args to datagen

--- sw/apps/common.mk
+++ sw/apps/common.mk
@@ -12,12 +12,14 @@ SECTION         ?=
 DATA_H          := $($(APP)_BUILD_DIR)/data.h
 DATAGEN_PY       = $(SCRIPTS_DIR)/datagen.py
 
-$(APP)_HEADERS := $(DATA_H)
-$(APP)_INCDIRS += $(dir $(DATA_H)) $(SRC_DIR)
+$(APP)_HEADERS      := $(DATA_H)
+$(APP)_INCDIRS      += $(dir $(DATA_H)) $(SRC_DIR)
+$(APP)_DATAGEN_ARGS += -c $($(APP)_DATA_CFG)
+$(APP)_DATAGEN_ARGS += --section="$(SECTION)"
 
 $(dir $(DATA_H)):
 	mkdir -p $@
 
-$(DATA_H): DATA_CFG := $($(APP)_DATA_CFG)
+$(DATA_H): DATAGEN_ARGS := $($(APP)_DATAGEN_ARGS)
 $(DATA_H): $(DATAGEN_PY) $($(APP)_DATA_CFG) | $(dir $(DATA_H))
-	$< -c $(DATA_CFG) --section="$(SECTION)" $@
+	$< $(DATAGEN_ARGS) $@
