544744dad55bb6dc5aeabe6a65d713b8b0771d6a
Xiaoling Yi <143962462+xiaoling-yi@users.noreply.github.com>
2024-08-23 16:07:43 +0200

    delete redundant cfg and sw (#270)
    * delete redundant cfg and sw
    
    * add back streamer-gemm and streamer-gemm-add-c and add back ci for hw build
    
    * add dr test for kul cluster

--- target/snitch_cluster/sw/apps/snax-data-reshuffler/src/snax-data-reshuffler.c
+++ target/snitch_cluster/sw/apps/snax-data-reshuffler/src/snax-data-reshuffler.c
@@ -36,7 +36,7 @@ int main() {
 
     uint32_t data_reshuffler_cycle;
 
-    if (CORE_IDX == 0) {
+    if (CORE_IDX == 1) {
         // Set data-reshuffler configuration CSR
         set_data_reshuffler_csr(
             tempLoop0_in, tempLoop1_in, tempLoop2_in, tempLoop3_in,
