fab5253a4ec09565212d68b26298065bc11374d2
Ryan Antonio <ryan.antonio@esat.kuleuven.be>
2024-08-28 16:24:39 +0200

    Modify memories to defaul tc_sram (#290)

--- hw/snitch_cluster/src/snitch_data_mem.sv
+++ hw/snitch_cluster/src/snitch_data_mem.sv
@@ -33,9 +33,6 @@ module snitch_data_mem #(
 );
 
   for (genvar i = 0; i < NumTotalBanks; i++) begin: gen_banks
-
-`ifndef TARGET_TAPEOUT
-
     tc_sram_impl #(
       .NumWords   ( TCDMDepth         ),
       .DataWidth  ( NarrowDataWidth   ),
@@ -55,37 +52,6 @@ module snitch_data_mem #(
       .wdata_i    ( mem_wdata_i[i]    ),
       .rdata_o    ( mem_rdata_o[i]    )
     );
-
-`else
-
-    //----------------------------------------------------
-    // This is just a place holder for the synthesis tool
-    // The cache memory is wide hence we break it into two
-    //----------------------------------------------------
-
-    // Memory implementation for synthesis
-    // Converting the byte enable to bit enable
-    logic [NarrowDataWidth - 1 : 0] bit_en;
-
-    always_comb begin
-      for (int j = 0; j < NarrowDataWidth / 8; j = j + 1) begin
-        bit_en[j*8+:8] = {8{mem_be_i[i][j]}};
-      end
-    end
-
-    syn_data_mem i_data_mem(
-              .CLK    ( clk_i                               ),
-              .CEB    ( ~mem_cs_i[i]                        ),
-              .WEB    ( ~mem_wen_i[i]                       ),
-              .A      ( mem_add_i[i][$clog2(TCDMDepth)-1:0] ),
-              .D      ( mem_wdata_i[i]                      ),
-              .BWEB   ( ~bit_en                             ),
-              .RTSEL  ( 2'b01                               ),
-              .WTSEL  ( 2'b01                               ),
-              .Q      ( mem_rdata_o[i]                      )
-    );
-
-`endif
   end
 
 endmodule
