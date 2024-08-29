541337e92e4eeb06aab232ed561d0ffc96acd6b4
Luca Colagrande <luca.colagrande3@gmail.com>
2024-08-29 19:40:12 +0200

    target: Use Verilator to generate accurate dependency files

--- target/common/verilator.mk
+++ target/common/verilator.mk
@@ -2,6 +2,15 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
+VLT_TOP_MODULE = testharness
+VLT_RTL_PREREQ_FILE = $(VLT_BUILDDIR)/$(VLT_TOP_MODULE).d
+
+$(VLT_BUILDDIR):
+	mkdir -p $@
+
+# Generate RTL prerequisites
+$(eval $(call gen_rtl_prerequisites,$(VLT_RTL_PREREQ_FILE),$(VLT_BUILDDIR),$(VLT_BENDER),$(VLT_TOP_MODULE),$(BIN_DIR)/$(TARGET).vlt))
+
 # This target just redirects the verilator simulation binary.
 # On IIS machines, verilator needs to be built and run in
 # the oseda environment, which is why this is necessary.
@@ -10,7 +19,7 @@ $(BIN_DIR)/$(TARGET).vlt: $(BIN_DIR)/$(TARGET)_bin.vlt
 	@echo '$(VERILATOR_SEPP) $(realpath $<) $$(realpath $$1) $$2' >> $@
 	@chmod +x $@
 
-$(BIN_DIR)/$(TARGET)_bin.vlt: $(VLT_SOURCES) $(TB_CC_SOURCES) $(VLT_CC_SOURCES) $(VLT_BUILDDIR)/lib/libfesvr.a | $(BIN_DIR)
+$(BIN_DIR)/$(TARGET)_bin.vlt: $(TB_CC_SOURCES) $(VLT_CC_SOURCES) $(VLT_BUILDDIR)/lib/libfesvr.a | $(BIN_DIR)
 	$(VLT) $(shell $(BENDER) script verilator $(VLT_BENDER)) \
 		$(VLT_FLAGS) --Mdir $(VLT_BUILDDIR) \
 		-CFLAGS -std=c++20 \
@@ -18,10 +27,15 @@ $(BIN_DIR)/$(TARGET)_bin.vlt: $(VLT_SOURCES) $(TB_CC_SOURCES) $(VLT_CC_SOURCES) 
 		-CFLAGS -I$(TB_DIR) \
 		-CFLAGS -I$(MKFILE_DIR)test \
 		-j $(VLT_JOBS) \
-		-o ../$@ --cc --exe --build --top-module testharness $(TB_CC_SOURCES) $(VLT_CC_SOURCES) $(VLT_BUILDDIR)/lib/libfesvr.a
+		-o ../$@ --cc --exe --build --top-module $(VLT_TOP_MODULE) \
+		$(TB_CC_SOURCES) $(VLT_CC_SOURCES) $(VLT_BUILDDIR)/lib/libfesvr.a
 
 .PHONY: clean-vlt
 clean-vlt: clean-work
 	rm -rf $(BIN_DIR)/$(TARGET).vlt $(BIN_DIR)/$(TARGET)_bin.vlt $(VLT_BUILDDIR)
 
 clean: clean-vlt
+
+ifneq ($(filter-out clean%,$(MAKECMDGOALS)),)
+-include $(VLT_RTL_PREREQ_FILE)
+endif
