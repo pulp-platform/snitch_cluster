d6998644f18c20eea4d62eaff310fd1589e6df05
Fanchen Kong <100082872+Konste11ation@users.noreply.github.com>
2024-08-31 14:48:35 +0200

    Fanchen/add support floating cluster addr (#293)
    * Change the the computation of _cls_ptr
    
    * Add the support for different cluster has different number of cores
    
    * Remove the space
    
    ---------
    
    Co-authored-by: Yunhao Deng <IveanEx@outlook.com>

--- sw/snRuntime/src/start.c
+++ sw/snRuntime/src/start.c
@@ -8,9 +8,10 @@ static inline uint32_t snrt_cls_base_addr() {
     extern volatile uint32_t __cbss_start, __cbss_end;
     uint32_t cdata_size = ((uint32_t)&__cdata_end) - ((uint32_t)&__cdata_start);
     uint32_t cbss_size = ((uint32_t)&__cbss_end) - ((uint32_t)&__cbss_start);
-    uint32_t l1_end_addr = SNRT_TCDM_START_ADDR +
-                           snrt_cluster_idx() * SNRT_CLUSTER_OFFSET +
-                           SNRT_TCDM_SIZE;
+    // uint32_t l1_end_addr = SNRT_TCDM_START_ADDR +
+    //                        snrt_cluster_idx() * SNRT_CLUSTER_OFFSET +
+    //                        SNRT_TCDM_SIZE;
+    uint32_t l1_end_addr = snrt_cluster_base_addrl() + SNRT_TCDM_SIZE;
     return l1_end_addr - cdata_size - cbss_size;
 }
 #endif
