e7a4839897aed1968e7ea1b5dae11b40ab32302e
Tim Fischer <fischeti@iis.ee.ethz.ch>
2024-09-04 20:33:07 +0200

    ci: Cache verilator build

--- target/common/verilator.mk
+++ target/common/verilator.mk
@@ -2,6 +2,14 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
+VLT_FLIST = $(VLT_SOURCES)
+VLT_FLIST += $(TB_CC_SOURCES)
+VLT_FLIST += $(VLT_CC_SOURCES)
+
+.PHONY: vlt-flist
+vlt-flist:
+	@echo $(VLT_FLIST)
+
 $(BIN_DIR)/$(TARGET).vlt: $(VLT_SOURCES) $(TB_CC_SOURCES) $(VLT_CC_SOURCES) $(VLT_BUILDDIR)/lib/libfesvr.a | $(BIN_DIR)
 	$(VLT) $(shell $(BENDER) script verilator $(VLT_BENDER)) \
 		$(VLT_FLAGS) -Mdir $(VLT_BUILDDIR) \
