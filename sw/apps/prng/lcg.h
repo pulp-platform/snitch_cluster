aeb8037c0e0e82c593ca377a6beb4ef5a2a235c2
Luca Colagrande <luca.colagrande3@gmail.com>
2024-10-11 18:32:34 +0200

    sw: Streamline PRNG API

--- sw/apps/prng/lcg.h
+++ sw/apps/prng/lcg.h
@@ -14,50 +14,53 @@
 #define LCG_A 1664525
 #define LCG_C 1013904223
 
+typedef struct {
+    uint32_t state;
+    uint32_t A;
+    uint32_t C;
+} lcg_t;
+
 __thread double max_uint_plus_1_inverse = (double)1.0 / (double)MAX_UINT_PLUS1;
 
-// Calculate A' and C' constants
-inline void leapfrog_constants(unsigned int num_sequences, uint32_t a,
-                               uint32_t c, uint32_t* Ap, uint32_t* Cp) {
-    // Ap = a^k
-    // Cp = (a^(k-1) + a^(k-2) + ... + a^1 + a^0)*c
-    uint32_t Ap_tmp = 1;
-    uint32_t Cp_tmp = 0;
-    for (unsigned int p = 0; p < num_sequences; p++) {
-        Cp_tmp += Ap_tmp;
-        Ap_tmp *= a;
-    }
-    Cp_tmp *= c;
-
-    // Store temporary variables to outputs
-    *Ap = Ap_tmp;
-    *Cp = Cp_tmp;
+lcg_t lcg_init_default(uint32_t seed) {
+    lcg_t lcg = {.state = seed, .A = LCG_A, .C = LCG_C};
+    return lcg;
 }
 
-// Calculate seed for leapfrog method's right sequence of index `sequence_idx`
-inline uint32_t right_seed(uint32_t left_seed, uint32_t a, uint32_t c,
-                           unsigned int sequence_idx) {
-    uint32_t seed = left_seed;
-    for (unsigned int p = 1; p <= sequence_idx; p++) {
-        seed = seed * a + c;
-    }
-    return seed;
+lcg_t lcg_init(uint32_t seed, uint32_t A, uint32_t C) {
+    lcg_t lcg = {.state = seed, .A = A, .C = C};
+    return lcg;
 }
 
-// Generate next PRN from LCG recurrence equation
-inline uint32_t lcg(uint32_t a, uint32_t c, uint32_t previous) {
-    return previous * a + c;
+uint32_t lcg_next(lcg_t* lcg) {
+    lcg->state = lcg->state * lcg->A + lcg->C;
+    return lcg->state;
 }
 
-// Normalize LCG PRN to [0, 1) range
-inline double normalize(uint32_t x) {
+// Normalize integer PRN to [0, 1) range
+double rand_int_to_unit_double(uint32_t x) {
     return (double)x * max_uint_plus_1_inverse;
 }
 
-inline void init_2d_lcg_params(unsigned int num_sequences, uint32_t seed,
-                               uint32_t a, uint32_t c, uint32_t* seed0,
-                               uint32_t* seed1, uint32_t* Ap, uint32_t* Cp) {
-    *seed0 = right_seed(seed, a, c, 2 * snrt_global_compute_core_idx());
-    *seed1 = right_seed(seed, a, c, 2 * snrt_global_compute_core_idx() + 1);
-    leapfrog_constants(num_sequences, a, c, Ap, Cp);
+void lcg_init_n(uint32_t seed, uint32_t A, uint32_t C, uint32_t n, lcg_t* lcg) {
+    // Calculate Ap and Cp of leapfrog sequences
+    // Ap = a^k
+    // Cp = (a^(k-1) + a^(k-2) + ... + a^1 + a^0)*c
+    uint32_t Ap = 1;
+    uint32_t Cp = 0;
+    for (unsigned int p = 0; p < n; p++) {
+        Cp += Ap;
+        Ap *= A;
+    }
+    Cp *= C;
+
+    // Calculate seeds of leapfrog sequences
+    lcg_t lcg0 = lcg_init(seed, A, C);
+    for (unsigned int i = 0; i < n; i++) {
+        lcg[i] = lcg_init(lcg_next(&lcg0), Ap, Cp);
+    }
+}
+
+void lcg_init_n_default(uint32_t seed, uint32_t n, lcg_t* lcg) {
+    lcg_init_n(seed, LCG_A, LCG_C, n, lcg);
 }
---
+++ sw/apps/prng/prng.h
@@ -0,0 +1,7 @@
+// Copyright 2023 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Luca Colagrande <colluca@iis.ee.ethz.ch>
+
+#include "lcg.h"
