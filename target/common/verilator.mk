f60f8a7cf053f02924b38c26260dbe72b10dca4e
Tim Fischer <fischeti@iis.ee.ethz.ch>
2025-01-24 14:23:07 +0100

    vlt: Wrap simulation binary in script
    This is required to run it on IIS machines since verilator currently can
    only be run in the oseda environment

--- target/common/verilator.mk
+++ target/common/verilator.mk
@@ -2,7 +2,15 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
-$(BIN_DIR)/$(TARGET).vlt: $(VLT_SOURCES) $(TB_CC_SOURCES) $(VLT_CC_SOURCES) $(VLT_BUILDDIR)/lib/libfesvr.a | $(BIN_DIR)
+# This target just redirects the verilator simulation binary.
+# On IIS machines, verilator needs to be built and run in
+# the oseda environment, which is why this is necessary.
+$(BIN_DIR)/$(TARGET).vlt: $(BIN_DIR)/$(TARGET)_bin.vlt
+	@echo "#!/bin/bash" > $@
+	@echo '$(VERILATOR_SEPP) $< $$(realpath $$1)' >> $@
+	@chmod +x $@
+
+$(BIN_DIR)/$(TARGET)_bin.vlt: $(VLT_SOURCES) $(TB_CC_SOURCES) $(VLT_CC_SOURCES) $(VLT_BUILDDIR)/lib/libfesvr.a | $(BIN_DIR)
 	$(VLT) $(shell $(BENDER) script verilator $(VLT_BENDER)) \
 		$(VLT_FLAGS) --Mdir $(VLT_BUILDDIR) \
 		-CFLAGS -std=c++20 \
@@ -14,6 +22,6 @@ $(BIN_DIR)/$(TARGET).vlt: $(VLT_SOURCES) $(TB_CC_SOURCES) $(VLT_CC_SOURCES) $(VL
 
 .PHONY: clean-vlt
 clean-vlt: clean-work
-	rm -rf $(BIN_DIR)/$(TARGET).vlt $(VLT_BUILDDIR)
+	rm -rf $(BIN_DIR)/$(TARGET).vlt $(BIN_DIR)/$(TARGET)_bin.vlt $(VLT_BUILDDIR)
 
 clean: clean-vlt
