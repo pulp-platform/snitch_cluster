cec020afe849c8b53807304e4b6e5ce520e0f33a
Yvan Tortorella <ytortorella@lagrev5.ee.ethz.ch>
2025-05-07 09:18:40 +0200

    Rsolve linter errors.

--- target/snitch_cluster/sw/apps/redmule/app.mk
+++ target/snitch_cluster/sw/apps/redmule/app.mk
@@ -1,6 +1,12 @@
-APP              := redmule
-$(APP)_BUILD_DIR := $(ROOT)/target/snitch_cluster/sw/apps/$(APP)/build
-SRCS             := $(ROOT)/target/snitch_cluster/sw/apps/$(APP)/src/$(APP).c
-$(APP)_INCDIRS   := $(ROOT)/target/snitch_cluster/sw/apps/$(APP)/data
+# Copyright 2023 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Andrea Belano <andrea.belano2@unibo.it>
 
-include $(ROOT)/target/snitch_cluster/sw/apps/common.mk
+APP              := redmule
+$(APP)_BUILD_DIR := $(SN_ROOT)/target/snitch_cluster/sw/apps/$(APP)/build
+SRCS             := $(SN_ROOT)/target/snitch_cluster/sw/apps/$(APP)/src/$(APP).c
+$(APP)_INCDIRS   := $(SN_ROOT)/target/snitch_cluster/sw/apps/$(APP)/data
+
+include $(SN_ROOT)/target/snitch_cluster/sw/apps/common.mk
