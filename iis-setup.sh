8ebaeeaed30277ea0e2513faaa0cea86eba9b5f5
Luca Colagrande <luca.colagrande3@gmail.com>
2025-07-02 09:20:53 +0200

    treewide: Separate kernel and docs Python dependencies

--- iis-setup.sh
+++ iis-setup.sh
@@ -20,5 +20,5 @@ source .venv/bin/activate
 # Also protects against "No space left on device" errors
 # occurring when the /tmp folder is filled by other processes.
 mkdir tmp
-TMPDIR=tmp pip install -e .
+TMPDIR=tmp pip install -e .[all]
 rm -rf tmp
