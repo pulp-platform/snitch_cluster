9528b4a6ab3f5275c72f6a29fba3c5404a4cad13
Luca Colagrande <luca.colagrande3@gmail.com>
2025-07-17 15:42:51 +0200

    runtime: Fix `snrt_wake_up` with fence

--- sw/snRuntime/src/start.c
+++ sw/snRuntime/src/start.c
@@ -70,11 +70,17 @@ static inline void snrt_init_bss() {
 
 #ifdef SNRT_WAKE_UP
 static inline void snrt_wake_up() {
+    // Core 0 of cluster 0 wakes up all other cores
     if (snrt_cluster_idx() == 0 && snrt_cluster_core_idx() == 0) {
         snrt_wake_all((1 << snrt_cluster_core_num()) - 1);
-    } else {
-        snrt_int_clr_mcip();
+        snrt_fence();
     }
+
+    // Synchronize all cores
+    snrt_cluster_hw_barrier();
+
+    // Clear the reset flag
+    snrt_int_clr_mcip();
 }
 #endif
 
