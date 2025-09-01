581c4e4cf5e00693842e348d2dfdadad77490a05
Luca Colagrande <luca.colagrande3@gmail.com>
2025-09-01 16:54:55 +0200

    Fix missing license header

--- make/traces.mk
+++ make/traces.mk
@@ -1,3 +1,9 @@
+# Copyright 2025 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Luca Colagrande <colluca@iis.ee.ethz.ch>
+
 ##########
 # Traces #
 ##########
