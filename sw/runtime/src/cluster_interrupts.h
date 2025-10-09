a21d505644df487c57210b539374943516df57b1
Luca Colagrande <luca.colagrande3@gmail.com>
2025-10-09 18:46:52 +0200

    sw/runtime: Extend `snrt_int_cluster_set` to multiple clusters

--- sw/runtime/src/cluster_interrupts.h
+++ sw/runtime/src/cluster_interrupts.h
@@ -9,9 +9,18 @@
 /**
  * @brief Write mask to the cluster-local interrupt set register
  * @param mask set bit at X sets the interrupt of hart X
+ * @param cluster_idx Index of the cluster to which the interrupt is sent
+ */
+inline void snrt_int_cluster_set(uint32_t mask, uint32_t cluster_idx) {
+    snrt_cluster(cluster_idx)->peripheral_reg.cl_clint_set.f.cl_clint_set = mask;
+}
+
+/**
+ * @brief Write mask to the cluster-local interrupt set register
+ * @param mask set bit at X sets the interrupt of hart X
  */
 inline void snrt_int_cluster_set(uint32_t mask) {
-    snrt_cluster()->peripheral_reg.cl_clint_set.f.cl_clint_set = mask;
+    snrt_int_cluster_set(mask, snrt_cluster_idx());
 }
 
 /**
