ef8805242b3d415bf2b9eed22fb7ac848748051b
Tim Fischer <fischeti@iis.ee.ethz.ch>
2025-11-20 11:22:52 +0100

    env: Use system-wide uv

--- iis-setup.sh
+++ iis-setup.sh
@@ -7,10 +7,6 @@
 export CC=gcc-9.2.0
 export CXX=g++-9.2.0
 export SN_BENDER=bender-0.28.1
-# TODO(fischeti): Install it locally? or tell IT to install it system-wide?
-export SN_UV="/home/fischeti/.local/bin/uv run --all-extras"
-export PATH=$PATH:/home/fischeti/.local/bin
-export UV_LINK_MODE=copy
 export SN_VCS_SEPP=vcs-2024.09
 export SN_VERILATOR_SEPP=oseda
 export SN_QUESTA_SEPP=questa-2023.4
@@ -18,3 +14,12 @@ export SN_LLVM_BINROOT=/usr/scratch2/vulcano/colluca/tools/riscv32-snitch-llvm-a
 
 # Add simulator binaries to PATH
 export PATH=$PWD/target/sim/build/bin:$PATH
+
+# For all python developments we use uv to setup the environment etc.,
+export SN_UV="/usr/local/uv/uv run --all-extras"
+export PATH="/usr/local/uv/:$PATH"
+# IT does not like if stuff is cached in home. We could use a temporary
+# directory, but since this script is not usually run numerous times, we
+# just disable the cache.
+# (see .gitlab-ci.yml for example of using smart caching with uv)
+export UV_NO_CACHE="true"
