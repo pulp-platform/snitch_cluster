3d8474e45318fd50299527feaddb2fd3b4a07e75
Luca Colagrande <luca.colagrande3@gmail.com>
2025-12-12 17:41:09 +0100

    Update TCDM test

--- sw/tests/src/tcdm.c
+++ sw/tests/src/tcdm.c
@@ -1,20 +1,12 @@
 // Copyright 2025 ETH Zurich and University of Bologna.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
-
+//
 // Author: Luca Colagrande <colluca@iis.ee.ethz.ch>
-
-// Test TCDM alignment functions
 
 #include "snrt.h"
 
-#define ALIGN_NEXT_FROM_BASE(addr, base, size) \
-    (((((addr) - (base)) + (size)-1) / (size)) * (size) + (base))
-
-#define BANK_ALIGNMENT 8
-#define TCDM_ALIGNMENT (48 * BANK_ALIGNMENT)
-#define ALIGN_UP_TCDM(addr) \
-    ALIGN_NEXT_FROM_BASE(addr, SNRT_TCDM_START_ADDR, TCDM_ALIGNMENT)
+#define TCDM_ALIGNMENT (SNRT_TCDM_BANK_NUM * SNRT_TCDM_BANK_WIDTH)
 
 int main() {
     if (snrt_cluster_core_idx() != 0) {
@@ -23,11 +15,9 @@ int main() {
 
     int n_errors = 1;
 
-    uintptr_t tcdm_base = (uintptr_t)snrt_l1_next();
-    uintptr_t tcdm_next_aligned = ALIGN_UP_TCDM(tcdm_base + 1);
-
-    uintptr_t exp = tcdm_base + TCDM_ALIGNMENT;
-    n_errors -= (tcdm_next_aligned == exp);
+    uintptr_t tcdm_base = SNRT_TCDM_START_ADDR;
+    uintptr_t actual = snrt_align_up(tcdm_base + 1, TCDM_ALIGNMENT);
+    n_errors -= actual == (tcdm_base + TCDM_ALIGNMENT);
 
     return n_errors;
 }
