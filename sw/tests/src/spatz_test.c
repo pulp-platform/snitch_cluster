b9bb30405de00c8b30117d8a17611c71d081f241
Diyou Shen <dishen@iis.ee.ethz.ch>
2026-05-14 14:43:26 +0200

    [SW] Add a simple spatz test.

---
+++ sw/tests/src/spatz_test.c
@@ -0,0 +1,60 @@
+// Copyright 2024 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <snrt.h>
+#include <stdint.h>
+
+#define N 8
+
+// Input data in off-chip memory (DRAM)
+double a[N] __attribute__((aligned(4096))) = {1.0, 1.0, 1.0, 1.0,
+                                              1.0, 1.0, 1.0, 1.0};
+double b[N] __attribute__((aligned(4096))) = {1.0, 1.0, 1.0, 1.0,
+                                              1.0, 1.0, 1.0, 1.0};
+
+int main() {
+  // Allocate space for a, b, c in L1 (TCDM)
+  double *local_a = (double *)snrt_l1_next();
+  double *local_b = local_a + N;
+  double *local_c = local_b + N;
+
+  // DMA core moves data from DRAM to L1
+  if (snrt_is_dm_core()) {
+    snrt_dma_start_1d(local_a, a, N * sizeof(double));
+    snrt_dma_start_1d(local_b, b, N * sizeof(double));
+    snrt_dma_wait_all();
+  }
+
+  snrt_cluster_hw_barrier();
+
+  if (snrt_is_compute_core()) {
+    int errs = 0;
+    unsigned int vl;
+
+    asm volatile("vsetvli %0, %1, e64, m8, ta, ma"
+                 : "=r"(vl)
+                 : "r"((unsigned int)N));
+
+    // Load a and b from L1, multiply, store result to L1
+    asm volatile("vle64.v v0, (%0)" ::"r"(local_a));
+    asm volatile("vle64.v v8, (%0)" ::"r"(local_b));
+    asm volatile("vfmul.vv v8, v0, v8");
+    asm volatile("vse64.v v8, (%0)" ::"r"(local_c));
+
+    // Check results via volatile pointer to prevent auto-vectorization
+    volatile double *vc = (volatile double *)local_c;
+    for (int i = 0; i < N; i++) {
+      if (vc[i] != 1.0)
+        errs++;
+    }
+
+    printf("Error:%d\n", errs);
+
+    // return errs;
+  }
+
+  snrt_cluster_hw_barrier();
+
+  return 0;
+}
