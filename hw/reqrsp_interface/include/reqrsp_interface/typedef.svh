9fb8e754efbfd62786c283d3e730111e2e2340ed
Luca Colagrande <luca.colagrande3@gmail.com>
2026-07-24 19:20:19 +0200

    reqrsp: Extend generic reqrsp typedefs

--- hw/reqrsp_interface/include/reqrsp_interface/typedef.svh
+++ hw/reqrsp_interface/include/reqrsp_interface/typedef.svh
@@ -39,11 +39,17 @@
     logic        q_ready;                       \
   }
 
-`define REQRSP_TYPEDEF_REQ_T(__req_t, __req_chan_t) \
+`define GENERIC_REQRSP_TYPEDEF_REQ_T(__req_t, __req_chan_t) \
   typedef `GENERIC_REQRSP_REQ_STRUCT(__req_chan_t) __req_t;
 
-`define REQRSP_TYPEDEF_RSP_T(__rsp_t, __rsp_chan_t) \
+`define GENERIC_REQRSP_TYPEDEF_RSP_T(__rsp_t, __rsp_chan_t) \
   typedef `GENERIC_REQRSP_RSP_STRUCT(__rsp_chan_t) __rsp_t;
+
+`define REQRSP_TYPEDEF_REQ_T(__req_t, __req_chan_t) \
+  `GENERIC_REQRSP_TYPEDEF_REQ_T(__req_t, __req_chan_t)
+
+`define REQRSP_TYPEDEF_RSP_T(__rsp_t, __rsp_chan_t) \
+  `GENERIC_REQRSP_TYPEDEF_RSP_T(__rsp_t, __rsp_chan_t)
 
 `define REQRSP_TYPEDEF_ALL(__name, __addr_t, __data_t, __strb_t, __user_t) \
   `REQRSP_TYPEDEF_REQ_CHAN_T(__name``_req_chan_t, __addr_t, __data_t, __strb_t, __user_t) \
