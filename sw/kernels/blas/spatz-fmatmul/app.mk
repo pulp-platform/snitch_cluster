f1de8894208c1d422aa1b04f7f935f74383e8a51
Luca Colagrande <luca.colagrande3@gmail.com>
2026-08-01 14:11:55 +0200

    Fix REUSE lint

--- sw/kernels/blas/spatz-fmatmul/app.mk
+++ sw/kernels/blas/spatz-fmatmul/app.mk
@@ -1,3 +1,7 @@
+# Copyright 2026 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
 APP              := spatz-fmatmul
 $(APP)_BUILD_DIR ?= $(SN_ROOT)/sw/kernels/blas/$(APP)/build
 SRC_DIR          := $(SN_ROOT)/sw/kernels/blas/$(APP)/src
