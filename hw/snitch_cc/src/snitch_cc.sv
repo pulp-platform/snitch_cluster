2735322975c3f1bca53d2ec6ac60b889fb30a365
gbellocchi <gbellocchi95@gmail.com>
2026-09-22 11:15:28 +0200

    hw: Remove `idma` cosmetic parameters from `snitch_cc`

--- hw/snitch_cc/src/snitch_cc.sv
+++ hw/snitch_cc/src/snitch_cc.sv
@@ -598,8 +598,6 @@ module snitch_cc
       .NumAxInFlight    (DmaNumAxInFlight),
       .DMAReqFifoDepth  (DmaReqFifoDepth),
       .NumChannels      (DmaNumChannels),
-      .TCDMAliasEnable  (TcdmAliasEnable),
-      .DMATracing       (1),
       .axi_ar_chan_t    (axi_ar_chan_t),
       .axi_aw_chan_t    (axi_aw_chan_t),
       .axi_req_t        (axi_req_t),
